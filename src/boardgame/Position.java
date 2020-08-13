ec522bb4a4efa8d4fe662b30523956b9829f75af
Rui Ribeiro <rui_rosendo15@hotmail.com>
2020-08-13 14:39:30 +0100

    Class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Position {
+
+	private Integer row;
+	private Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+	
+	public void setValues(Integer row, Integer column) {
+		this.row  = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
